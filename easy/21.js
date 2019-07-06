480b546e2e3870e3c7be80b4e3ad90c2a745f339
mras2303 <marek.kowalonek@gmail.com>
2019-07-07 00:55:59 +0400

    Update 21.js

--- easy/21.js
+++ easy/21.js
@@ -2,10 +2,10 @@ function chatroomStatus(users) {
 	
 }
 
-Test.assertSimilar(chatroomStatus([]), "no one online")
-Test.assertSimilar(chatroomStatus(["becky325"]), "becky325 online")
-Test.assertSimilar(chatroomStatus(["becky325", "malcolm888"]), "becky325 and malcolm888 online")
-Test.assertSimilar(chatroomStatus(["becky325", "malcolm888", "fah32fa"]), "becky325, malcolm888 and 1 more online")
-Test.assertSimilar(chatroomStatus(["paRIE_to"]), "paRIE_to online")
-Test.assertSimilar(chatroomStatus(["s234f", "mailbox2"]), "s234f and mailbox2 online")
-Test.assertSimilar(chatroomStatus(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]), "pap_ier44, townieBOY and 4 more online")
+testSimilar(chatroomStatus([]), "no one online")
+testSimilar(chatroomStatus(["becky325"]), "becky325 online")
+testSimilar(chatroomStatus(["becky325", "malcolm888"]), "becky325 and malcolm888 online")
+testSimilar(chatroomStatus(["becky325", "malcolm888", "fah32fa"]), "becky325, malcolm888 and 1 more online")
+testSimilar(chatroomStatus(["paRIE_to"]), "paRIE_to online")
+testSimilar(chatroomStatus(["s234f", "mailbox2"]), "s234f and mailbox2 online")
+testSimilar(chatroomStatus(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]), "pap_ier44, townieBOY and 4 more online")

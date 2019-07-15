8c0b4c51e337aae063d1a5e0ad57107227c19f45
mras2303 <marek.kowalonek@gmail.com>
2019-07-15 16:08:50 +0200

    Update 10.js

--- easy/10.js
+++ easy/10.js
@@ -1,11 +1,9 @@
-function divisibleByFive(n) {
-
+function largestSwap(num) {
+	
 }
 
-test(false, divisibleByFive(7))
-test(true, divisibleByFive(5))
-test(true, divisibleByFive(15))
-test(false, divisibleByFive(33))
-test(false, divisibleByFive(-18))
-test(false, divisibleByFive(999))
-test(false, divisibleByFive(2))
+test(largestSwap(27), false)
+test(largestSwap(43), true)
+test(largestSwap(14), false)
+test(largestSwap(53), true)
+test(largestSwap(99), true)

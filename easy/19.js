2ac53347b6cd9f21b385e6092f1715cb1c735763
Tubka <Tubka94@users.noreply.github.com>
2019-07-24 17:42:36 +0200

    add 19

--- easy/19.js
+++ easy/19.js
@@ -1,9 +1,9 @@
 function timeForMilkAndCookies(date) {
-	
+  return date.getDate() == 24 && date.getMonth() == 11;
 }
 
-test(timeForMilkAndCookies(new Date(2013, 11, 24)), true)
-test(timeForMilkAndCookies(new Date(3000, 11, 24)), true)
-test(timeForMilkAndCookies(new Date(2154, 11, 11)), false)
-test(timeForMilkAndCookies(new Date(2010, 11, 2)), false)
-test(timeForMilkAndCookies(new Date(1980, 9, 24)), false)
+test(timeForMilkAndCookies(new Date(2013, 11, 24)), true);
+test(timeForMilkAndCookies(new Date(3000, 11, 24)), true);
+test(timeForMilkAndCookies(new Date(2154, 11, 11)), false);
+test(timeForMilkAndCookies(new Date(2010, 11, 2)), false);
+test(timeForMilkAndCookies(new Date(1980, 9, 24)), false);

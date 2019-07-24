91329cdbc224577e75aefdea4b8690186baedc49
Tubka <Tubka94@users.noreply.github.com>
2019-07-24 13:31:55 +0200

    add 9

--- easy/9.js
+++ easy/9.js
@@ -1,13 +1,18 @@
 function hashPlusCount(str) {
-	
+  let hash = (plus = 0);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "#") hash++;
+    else if (str[i] == "+") plus++;
+  }
+  return `${hash},${plus}`;
 }
 
-testSimilar(hashPlusCount("####"), [4, 0])
-testSimilar(hashPlusCount("#"), [1, 0])
-testSimilar(hashPlusCount("+++++++"), [0, 7])
-testSimilar(hashPlusCount("++"), [0, 2])
-testSimilar(hashPlusCount("#+#+"), [2, 2])
-testSimilar(hashPlusCount("###+"), [3, 1])
-testSimilar(hashPlusCount("##+++#"), [3, 3])
-testSimilar(hashPlusCount("#+++#+#++#"), [4, 6])
-testSimilar(hashPlusCount(""), [0, 0])
+testSimilar(hashPlusCount("####"), [4, 0]);
+testSimilar(hashPlusCount("#"), [1, 0]);
+testSimilar(hashPlusCount("+++++++"), [0, 7]);
+testSimilar(hashPlusCount("++"), [0, 2]);
+testSimilar(hashPlusCount("#+#+"), [2, 2]);
+testSimilar(hashPlusCount("###+"), [3, 1]);
+testSimilar(hashPlusCount("##+++#"), [3, 3]);
+testSimilar(hashPlusCount("#+++#+#++#"), [4, 6]);
+testSimilar(hashPlusCount(""), [0, 0]);

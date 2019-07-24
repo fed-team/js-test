b6ad72f64e7c28bb2cc06682349076e9123fb986
Tubka <Tubka94@users.noreply.github.com>
2019-07-24 13:40:19 +0200

    add 13,15

--- easy/13.js
+++ easy/13.js
@@ -1,17 +1,20 @@
 function isPrefix(word, prefix) {
-	
+  return word.slice(0, prefix.length - 1) == prefix.slice(0, -1);
 }
 
 function isSuffix(word, suffix) {
-	
+  return (
+    word.slice(word.length - suffix.length + 1, word.length) ==
+    suffix.slice(1, suffix.length)
+  );
 }
 
-test(isPrefix('automation', 'auto-'), true)
-test(isPrefix('superfluous', 'super-'), true)
-test(isPrefix('oration', 'mega-'), false)
-test(isPrefix('retrospect', 'sub-'), false)
+test(isPrefix("automation", "auto-"), true);
+test(isPrefix("superfluous", "super-"), true);
+test(isPrefix("oration", "mega-"), false);
+test(isPrefix("retrospect", "sub-"), false);
 
-test(isSuffix('arachnophobia', '-phobia'), true)
-test(isSuffix('rhinoplasty', '-plasty'), true)
-test(isSuffix('movement', '-scope'), false)
-test(isSuffix('vocation', '-logy'), false)
+test(isSuffix("arachnophobia", "-phobia"), true);
+test(isSuffix("rhinoplasty", "-plasty"), true);
+test(isSuffix("movement", "-scope"), false);
+test(isSuffix("vocation", "-logy"), false);
--- easy/15.js
+++ easy/15.js
@@ -1,9 +1,13 @@
 function isOmnipresent(arr, val) {
-	
+  let num = 0;
+  arr.forEach(el => {
+    if (el.includes(val)) num++;
+  });
+  return num == arr.length;
 }
 
-test(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 1), true)
-test(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6), false)
-test(isOmnipresent([[5], [5], [5], [6, 5]], 5), true)
-test(isOmnipresent([[5], [5], [5], [6, 5]], 6), false)
-test(isOmnipresent([[5, 1], [5, 1], [5, 1], [6, 5, 1]], 1), true)
+test(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 1), true);
+test(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6), false);
+test(isOmnipresent([[5], [5], [5], [6, 5]], 5), true);
+test(isOmnipresent([[5], [5], [5], [6, 5]], 6), false);
+test(isOmnipresent([[5, 1], [5, 1], [5, 1], [6, 5, 1]], 1), true);

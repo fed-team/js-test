2f46f07e8ac485ddadb865890456b49b295a8f0d
Tubka <Tubka94@users.noreply.github.com>
2019-07-24 13:34:08 +0200

    add 10, 11

--- easy/10.js
+++ easy/10.js
@@ -1,9 +1,9 @@
 function largestSwap(num) {
-	
+  return num.toString()[1] + num.toString()[0] <= num;
 }
 
-test(largestSwap(27), false)
-test(largestSwap(43), true)
-test(largestSwap(14), false)
-test(largestSwap(53), true)
-test(largestSwap(99), true)
+test(largestSwap(27), false);
+test(largestSwap(43), true);
+test(largestSwap(14), false);
+test(largestSwap(53), true);
+test(largestSwap(99), true);

ed15dde5e626640f59978f3c692f0b50be175616
Tubka <Tubka94@users.noreply.github.com>
2019-07-25 16:13:24 +0200

    add 23, 24

--- easy/23.js
+++ easy/23.js
@@ -1,8 +1,10 @@
 function factorial(n) {
-	
+  let strong = 1;
+  for (let i = 1; i <= n; i++) strong = strong * i;
+  return strong;
 }
 
-test(factorial(5), 120)
-test(factorial(3), 6)
-test(factorial(1), 1)
-test(factorial(0), 1)
+test(factorial(5), 120);
+test(factorial(3), 6);
+test(factorial(1), 1);
+test(factorial(0), 1);

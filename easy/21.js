8e162470068c08209b8aaca628431459c47035d5
Tubka <Tubka94@users.noreply.github.com>
2019-07-25 15:31:53 +0200

    add 20, 21, 22

--- easy/21.js
+++ easy/21.js
@@ -1,11 +1,35 @@
 function chatroomStatus(users) {
-	
+  if (users.length == 0) {
+    return `no one online`;
+  } else if (users.length == 1) {
+    return `${users[0]} online`;
+  } else if (users.length == 2) {
+    return `${users[0]} and ${users[1]} online`;
+  } else if (users.length >= 3) {
+    return `${users[0]}, ${users[1]} and ${users.length - 2} more online`;
+  }
 }
 
-testSimilar(chatroomStatus([]), "no one online")
-testSimilar(chatroomStatus(["becky325"]), "becky325 online")
-testSimilar(chatroomStatus(["becky325", "malcolm888"]), "becky325 and malcolm888 online")
-testSimilar(chatroomStatus(["becky325", "malcolm888", "fah32fa"]), "becky325, malcolm888 and 1 more online")
-testSimilar(chatroomStatus(["paRIE_to"]), "paRIE_to online")
-testSimilar(chatroomStatus(["s234f", "mailbox2"]), "s234f and mailbox2 online")
-testSimilar(chatroomStatus(["pap_ier44", "townieBOY", "panda321", "motor_bike5", "sandwichmaker833", "violinist91"]), "pap_ier44, townieBOY and 4 more online")
+testSimilar(chatroomStatus([]), "no one online");
+testSimilar(chatroomStatus(["becky325"]), "becky325 online");
+testSimilar(
+  chatroomStatus(["becky325", "malcolm888"]),
+  "becky325 and malcolm888 online"
+);
+testSimilar(
+  chatroomStatus(["becky325", "malcolm888", "fah32fa"]),
+  "becky325, malcolm888 and 1 more online"
+);
+testSimilar(chatroomStatus(["paRIE_to"]), "paRIE_to online");
+testSimilar(chatroomStatus(["s234f", "mailbox2"]), "s234f and mailbox2 online");
+testSimilar(
+  chatroomStatus([
+    "pap_ier44",
+    "townieBOY",
+    "panda321",
+    "motor_bike5",
+    "sandwichmaker833",
+    "violinist91"
+  ]),
+  "pap_ier44, townieBOY and 4 more online"
+);
--- easy/22.js
+++ easy/22.js
@@ -1,12 +1,15 @@
 function charCount(char, str) {
-	
+  return [...str].filter(oneLetter => oneLetter == char).length;
 }
 
-test(charCount('a', 'edabit'), 1)
-test(charCount('b', 'big fat bubble'), 4)
-test(charCount('c', 'Chamber of secrets'), 1)
-test(charCount('f', 'frank and his friends have offered five foxes for sale'), 7)
-test(charCount('x', 'edabit'), 0)
-test(charCount('a', 'Adam and Eve bit the apple and found a snake'), 6)
-test(charCount('s', 'sssssssssssssssssssssssss'), 25)
-test(charCount('7', '10795426697'), 2)
+test(charCount("a", "edabit"), 1);
+test(charCount("b", "big fat bubble"), 4);
+test(charCount("c", "Chamber of secrets"), 1);
+test(
+  charCount("f", "frank and his friends have offered five foxes for sale"),
+  7
+);
+test(charCount("x", "edabit"), 0);
+test(charCount("a", "Adam and Eve bit the apple and found a snake"), 6);
+test(charCount("s", "sssssssssssssssssssssssss"), 25);
+test(charCount("7", "10795426697"), 2);

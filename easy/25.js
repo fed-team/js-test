00fa6141a1f357a71dabf80ab120e2626d815a4c
Tubka <Tubka94@users.noreply.github.com>
2019-07-27 03:13:50 +0200

    update 25

--- easy/25.js
+++ easy/25.js
@@ -3,7 +3,7 @@ function isZipCode(zipcode) {
 }
 
 test(isZipCode("81-442"), true);
-test(isZipCode("81442")), false;
+test(isZipCode("81442"), false);
 test(isZipCode("33-789"), true);
 test(isZipCode("42-531"), true);
 test(isZipCode("9a3-45"), false);

1664eda86a7fac9b6c436da61d6f5dcfd5403ef7
Iicytower <szymonfirkowicz@gmail.com>
2019-10-11 13:22:30 +0200

    done easy?only easy, i stil didnt do regex

--- easy/8.js
+++ easy/8.js
@@ -1,11 +1,6 @@
 function societyName(friends) {
-    friends.sort();
-    let name = "";
-    friends.map(el => {
-        name += el.charAt(0);
-    });
 
-    return name;
+    return friends.sort().reduce((acc, el) => acc + el.charAt(0), '');
 }
 
 test(societyName(['Adam', 'Sarah', 'Malcolm']), 'AMS')
